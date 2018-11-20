0538f87e999594d94d63e56bc74340d56fd80a18
Dan Leatherman <dleatherman@gmail.com>
2018-11-20 10:45:47 -0500

    more animation

--- 10/js/scripts.js
+++ 10/js/scripts.js
@@ -23,9 +23,21 @@ window.onload = function () {
 
   inView('#section-2')
     .on('enter', el => {
+
+      el.className = 'active';
+
+      // c key for circle
+      const circle = document.createElement('div');
+      circle.className = 'circle';
+      circle.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
+      circle.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
+      el.appendChild(circle);
+
       el.style.backgroundColor = 'blue';
     })
     .on('exit', el => {
+      // alert('GONE');
+      el.className = '';
       el.style.backgroundColor = 'lemonchiffon';
     })
 
