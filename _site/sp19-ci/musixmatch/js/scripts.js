eb4b7c0006576c27509af60858830bb0bc533cec
Dan Leatherman <dleatherman@gmail.com>
2019-04-26 08:43:50 -0400

    cors allow

--- _site/sp19-ci/musixmatch/js/scripts.js
+++ _site/sp19-ci/musixmatch/js/scripts.js
@@ -3,7 +3,7 @@
    const APIKEY = '53f978ba680e6656fa6ae8444295da63';
 
    window.onload = () => {
-      fetch(`https://api.musixmatch.com/ws/1.1/?apiKey=${APIKEY}`, {
+      fetch(`https://api.musixmatch.com/ws/1.1/?apiKey=${APIKEY}&artist.search?q_artist=khalid&page_size=5`, {
          mode: 'cors'
       })
          .then(response => {
