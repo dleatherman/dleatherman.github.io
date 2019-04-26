2daa425a0c1f745209f6a91e7f0e0edcde6557c0
Dan Leatherman <dleatherman@gmail.com>
2019-04-26 10:41:27 -0400

    album list

--- sp19-ci/musixmatch/js/scripts.js
+++ sp19-ci/musixmatch/js/scripts.js
@@ -3,7 +3,8 @@
    const APIKEY = '53f978ba680e6656fa6ae8444295da63',
       proxy = 'https://cors-anywhere.herokuapp.com/',
       artistForm = document.querySelector('#artist__form'),
-      artistSelect = document.querySelector('#artist__select');
+      artistSelect = document.querySelector('#artist__select'),
+      albumListWrapper = document.querySelector('#album__list');
 
    const getArtistInfo = (event) => {
 
@@ -14,10 +15,21 @@
          return;
       }
 
-      fetch(`${proxy}http://api.musixmatch.com/ws/1.1/artist.get?artist_id=${artistSelect.value}&apikey=${APIKEY}`)
+      fetch(`${proxy}http://api.musixmatch.com/ws/1.1/artist.albums.get?artist_id=${artistSelect.value}&s_release_date=desc&g_album_name=1&apikey=${APIKEY}`)
          .then(response => response.json())
          .then(data => {
-            console.log(data);
+            if (!data) {
+               throw new Error(data.message.header.statusText || 'Unknown Error');
+            }
+            return data.message.body.album_list;
+         })
+         .then(albums => {
+            let albumList = '<ul>';
+            for (let i = 0; i < albums.length; i++) {
+               albumList += `<li>${albums[i].album.album_name}</li>`;
+            }
+            albumList += '</ul>';
+            albumListWrapper.innerHTML = albumList;
          })
          .catch(error => console.error(error));
    };
