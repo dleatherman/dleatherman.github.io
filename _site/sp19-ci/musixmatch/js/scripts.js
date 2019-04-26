668e33f4b43485976870e85cfce4584b29505211
Dan Leatherman <dleatherman@gmail.com>
2019-04-26 10:16:58 -0400

    musixmatch api example

--- _site/sp19-ci/musixmatch/js/scripts.js
+++ _site/sp19-ci/musixmatch/js/scripts.js
@@ -1,16 +1,29 @@
 (function () {
 
-   const APIKEY = '53f978ba680e6656fa6ae8444295da63';
+   const APIKEY = '53f978ba680e6656fa6ae8444295da63',
+      proxy = 'https://cors-anywhere.herokuapp.com/',
+      artistForm = document.querySelector('#artist__form'),
+      artistSelect = document.querySelector('#artist__select');
 
-   window.onload = () => {
-      fetch(`https://api.musixmatch.com/ws/1.1/?apiKey=${APIKEY}&artist.search?q_artist=khalid&page_size=5`, {
-         mode: 'cors'
-      })
-         .then(response => {
-            console.log(response);
+   const getArtistInfo = (event) => {
+
+      event.preventDefault();
+
+      if(!artistSelect.value) {
+         window.alert('Please select an artist');
+         return;
+      }
+
+      fetch(`${proxy}http://api.musixmatch.com/ws/1.1/artist.get?artist_id=${artistSelect.value}&apikey=${APIKEY}`)
+         .then(response => response.json())
+         .then(data => {
+            console.log(data);
          })
          .catch(error => console.error(error));
-   }
+   };
 
+   if(artistForm) {
+      artistForm.addEventListener('submit', getArtistInfo);
+   }
 
 }());
